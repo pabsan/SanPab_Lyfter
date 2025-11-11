77bf9b44807148dc7aff2ac4d32963fb426bc2aa
Pablo <psanchezcr@gmail.com>
2025-11-10 21:31:31 -0600

    Principal menu

---
+++ Semana 10/main.py
@@ -0,0 +1,6 @@
+import menu
+
+if __name__ == "__main__":
+    option = True
+    while option == True:
+        option = menu.choose_option()
---
+++ Semana 10/menu.py
@@ -0,0 +1,37 @@
+import os
+
+def clear_screen():
+    os.system('cls')
+
+def draw_main_menu():
+    menu_options = {
+        1:'Enter Students Information',
+        2:'Check Students Information',
+        3:'Top 3 with Best Avg',
+        4:'Check All Avg',
+        5:'Export to CSV',
+        6:'Import from CSV',
+        0:'Exit'
+    }
+    
+    clear_screen()
+    print("=== Main Menu ===")
+    for key, value in menu_options.items():
+        print(f"{key}. {value}")
+    print("=================")
+    return menu_options
+
+
+def choose_option():
+    try:
+        isOn = True
+        options = draw_main_menu()
+        choice = int(input("Enter your choice: "))
+        if choice in options:
+             if choice == 0:
+                print("Goodbye!")
+                isOn = False
+        return isOn
+    except ValueError as e:
+        print(f"Error not an valid option. {e}")
+
