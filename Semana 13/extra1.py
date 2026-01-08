7be999656dab643d4e4e8ab8e1b3aaa85a512242
Pablo <psanchezcr@gmail.com>
2026-01-07 20:55:14 -0600

    Correcciones

--- Semana 13/extra1.py
+++ Semana 13/extra1.py
@@ -1,13 +1,25 @@
 def repeat_twice(func):
-    def wrapper(name):
-        print(f"Hello {name}!")
-        print(f"Hello {name}!")
-        return func(name)
+    def wrapper(*args, **kwargs):
+        result1 = func(*args, **kwargs)
+        result2 = func(*args, **kwargs)
+        return result1, result2
     return wrapper
 
 @repeat_twice
 def greet(name):
-    print(f"Nice to meet you, {name}!")
+    print(f"Hello, {name}!")
+
+
+@repeat_twice
+def subtract(a, b):
+    return a - b
+
+#save data
+@repeat_twice
+def save_data(data1, data2):
+    print(f"Saving data: {data1} and {data2}")
 
 greet("Alyson")
-
+result = subtract(10, 4)
+print("Result of subtraction:", result)  # Result of subtraction: 6
+save_data("Accoun1", "Account2")
--- Semana 13/extra2.py
+++ Semana 13/extra2.py
@@ -1,21 +1,25 @@
 user_logged_in = False
 
 def requires_login(func):
-    def wrapper():
-        if not user_logged_in:
-            raise PermissionError("Usuario no autenticado.")
-        return func()
+    def wrapper(*args, **kwargs):
+        for arg in args:
+            if not arg:
+                raise PermissionError("Usuario no autenticado.")
+        for key, value in kwargs.items():
+            if not value:
+                raise PermissionError("Usuario no autenticado.")
+        return func(*args, **kwargs)
     return wrapper
 
 
 @requires_login
-def view_profile():
+def view_profile(user):
     print("Mostrando perfil del usuario")
 
 try:
-    view_profile()
+    view_profile(user_logged_in)
 except PermissionError as e:
     print(e)
 
 user_logged_in = True
-view_profile()
+view_profile(user_logged_in)
