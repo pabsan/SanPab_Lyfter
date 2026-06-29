89c66e08eb901ccb8ec8d3739cc43d993223b3e6
Pablo <psanchezcr@gmail.com>
2026-06-28 20:52:59 -0600

    1

--- Flask/02/SQL/up/CREATE_TABLE_Usuarios.sql
+++ Flask/02/SQL/up/CREATE_TABLE_Usuarios.sql
@@ -14,7 +14,7 @@ DECLARE
     row_count INT;
 BEGIN
     -- 1. Store the count in a variable
-    SELECT COUNT(*) INTO row_count FROM Usuarios WHERE;
+    SELECT COUNT(*) INTO row_count FROM Usuarios;
 
     IF row_count = 0 THEN
         INSERT INTO usuarios
