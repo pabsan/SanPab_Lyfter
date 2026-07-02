1e609822c36c2976e0c0579e1d87eb0118a64ded
Pablo <psanchezcr@gmail.com>
2026-07-02 11:00:00 -0600

    Create NewUser procedure for user registration

---
+++ Flask/02/SQL/Scripts/NewUser.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE PROCEDURE NewUser(
+    p_name VARCHAR(100),
+    p_email VARCHAR(100),
+    p_username VARCHAR(50),
+    p_born_date DATE,
+    p_password VARCHAR(255),
+    p_status_cta VARCHAR(50),
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM Users
+        WHERE email = p_email OR username = p_username;
+    ) THEN
+        INSERT INTO Users (name, email, username, born_date, password, status_cta)
+        VALUES (p_name, p_email, p_username, p_username, p_born_date, p_password, p_status);
+    END IF;
+    COMMIT;
+END;
+$$;
