da1fb09265c7d3c549e68b11c4dade4d2bf60cf4
Pablo <psanchezcr@gmail.com>
2026-08-27 20:08:09 -0600

    update users

--- Flask/03/UserRepository.py
+++ Flask/03/UserRepository.py
@@ -1,5 +1,27 @@
 from sqlalchemy.orm import sessionmaker, session
+from Base import User
 
 class UserRepository:
     def __init__(self, session_factory: sessionmaker):
         self.session_factory = session_factory
+
+    def create(self, id: int, name: str, last_name: str, status: str, birth_date: str) -> User:
+        with self.session_factory() as session:
+            user = User(id=id, name=name, last_name=last_name, status=status, birth_date=birth_date)
+            session.add(user)
+            session.commit()
+            return user
+
+    def update(self, user_id: int, **kwargs) -> User:
+        with self.session_factory() as session:
+            user = session.get(User, user_id)
+            if not user:
+                raise ValueError(f"User with id {user_id} does not exist.")
+            else:
+                for key, value in kwargs.items():
+                    if hasattr(user, key):
+                        setattr(user, key, value)
+                    else:
+                        raise ValueError(f"User has no attribute '{key}'")
+                session.commit()
+                return user
--- Flask/03/setup.py
+++ Flask/03/setup.py
@@ -1,17 +1,24 @@
-from sqlalchemy import create_engine
-from Base import Base, User, Address, Car
+from multiprocessing import connection
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from Base import Base, User, Address, Car
+from UserRepository import UserRepository
 
 DB_URI = 'postgresql://postgres:postgres@localhost:5432/postgres'
 engine = create_engine(DB_URI, echo=True)
 
 try:
-    connection = engine.connect()
-    print("Connection successful!")
-
     #create the tables in the database
     Base.metadata.create_all(engine)
 
-    connection.close()  # Cerramos la conexion cuando terminamos
+    SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+
+    user_repo = UserRepository(SessionLocal)
+    #user1 = user_repo.create(id=2, name="John", last_name="Doe", status="active", birth_date="1990-01-01")
+    #print(f"User created: {user1}")
+
+    updated_user = user_repo.update(user_id=2, name="Maria")
+    print(f"Updated Name: {updated_user.name if updated_user else 'Failed'}")
 except Exception as e:
     print("Connection failed:", e)
