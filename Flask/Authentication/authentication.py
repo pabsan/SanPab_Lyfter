d52090760a5d169611c73a3fb4624e8e532a9faf
Pablo <psanchezcr@gmail.com>
2026-09-16 21:06:10 -0600

    last changes

--- Flask/Authentication/authentication.py
+++ Flask/Authentication/authentication.py
@@ -10,6 +10,47 @@ def liveness():
     return "<p>Hello World!</p>"
 
 
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    if (data.get('username') == None or data.get('password') == None):
+        return Response(status=400)
+    else:
+        result = db_manager.insert_user(data.get('username'),data.get('password'))
+        user_id = result[0]
+
+        token = jwt_manager.encode({'id':user_id})
+        return jsonify(token=token)
+
+@app.route("/login",methods=["POST"])
+def login():
+    data = request.get_json()
+    if (data.get('username') == None or data.get('password') == None):
+        return Response(status=400)
+    else:
+        result = db_manager.get_user(data.get('username'),data.get('password'))
+        if result == None:
+            return Response(status=403)
+        else:
+            user_id = result[0]
+            token = jwt_manager.encode({'id':user_id})
+            return jsonify(token=token)
+
+@app.route("/me")
+def me():
+    try:
+        token = request.headers.get("Authorization")
+        if token is not None:
+            token = token.replace("Bearer ","")
+            decoded = jwt_manager.decode(token)
+            user_id = decoded['id']
+            user = db_manager.get_user_by_id(user_id)
+            return jsonify(id=user_id, username = user[1])
+        else:
+            return Response(status=403)
+    except Exception as e:
+        return Response(status=500)
+
 if __name__ == "__main__":
     db_manager = DB_Manager()
     jwt_manager = JWT_Manager('trespatitos','HS256')
