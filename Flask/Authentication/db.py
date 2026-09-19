303c01cd52e6f943e521492f2e06cd9d58f1de34
Pablo <psanchezcr@gmail.com>
2026-09-18 21:04:51 -0600

    fist part

--- Flask/Authentication/db.py
+++ Flask/Authentication/db.py
@@ -1,17 +1,32 @@
+import datetime
+
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Table, Column, Integer, String, Numeric, DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+import numeric 
+
+
 from sqlalchemy import insert, select
 
 metadata_obj = MetaData()
 
-user_table = Table(
-    "users",
-    metadata_obj,
-    Column("id", Integer, primary_key=True),
-    Column("username",String(30)),
-    Column("password",String)
-)
+class Base(DeclarativeBase):
+    pass
+
+class users(Base):
+    __tablename__ = "users"
+    id : Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[String] = mapped_column(String(30))
+    password: Mapped[String] = mapped_column(String) 
+
+class products(Base):
+    __tablename__ = "products"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[String] = mapped_column(String(50))
+    price: Mapped[Numeric] = mapped_column(Numeric(10,2))
+    entry_date: Mapped[DateTime] = mapped_column(DateTime)
+    quatity: Mapped[int] = mapped_column(Integer)
 
 class DB_Manager:
     def __init__(self):
